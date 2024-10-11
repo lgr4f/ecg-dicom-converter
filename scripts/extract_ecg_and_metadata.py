379817108608df483c3baa70612799fa8e128bf5
Lennart Graf <frederiklennart.graf@med.uni-goettingen.de>
2024-10-11 12:22:52 +0200

    Handle missing 'PatientDemographics' section in XML to prevent crash, defaulting to warning

--- scripts/extract_ecg_and_metadata.py
+++ scripts/extract_ecg_and_metadata.py
@@ -3,7 +3,7 @@ import xml.etree.ElementTree as ET
 import base64
 import struct
 import numpy as np
-
+import warnings
 
 def extract_wfdb_data(file_path):
     record = wfdb.rdrecord(file_path[:-4])
@@ -72,14 +72,22 @@ def extract_muse_xml_data(file_path):
 
         # Extract additional metadata
         patient = root.find('.//PatientDemographics')
+
         test = root.find('.//TestDemographics')
         order = root.find('.//Order')
         measurements = root.find('.//RestingECGMeasurements')
+        if patient is not None:
+            metadata = {
+                'PatientID': patient.findtext('PatientID'),
+                'LeadFilters': lead_filters
+            }
+        else:
+            metadata = {
+                'PatientID': "",
+                'LeadFilters': lead_filters
+            }
+            warnings.warn("There is no PatientDemographics section in the XML")
 
-        metadata = {
-            'PatientID': patient.findtext('PatientID'),
-            'LeadFilters': lead_filters
-        }
 
         if patient is not None:
             metadata['PatientName'] = (
